5f104331d658e006f05c7cf909f99fe726a77d90
LucyT <lucy.tan@teselagen.com>
2018-11-20 11:04:56 -0800

    added line to check push

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            Addin in some words to check the push
           </p>
           <a
             className="App-link"
